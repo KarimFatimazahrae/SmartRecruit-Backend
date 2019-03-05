16b41b3ada17ad028a5078ad5aca340887c3850e
karim <karim.fatimazahrae@gmail.com>
2019-03-05 01:15:22 +0100

    script de la base de données et le fichier python de la connexon avec la BD

--- backend.py
+++ backend.py
@@ -20,60 +20,143 @@ mysql.init_app(app)
 # Api acces routes
 @app.route('/offers')
 def getOffers():
+	applicant = request.args.get('applicant')
 	try:
 	    cur = mysql.connect().cursor()
-	    cur.execute('''select * from offre''')
 	    jobOffers = []
+	    sql = '''SELECT * FROM offre WHERE id_offre NOT IN (SELECT id_offre FROM candidat_offre WHERE id_candidat=%s) AND id_offre NOT IN (SELECT id_offre FROM favoris WHERE id_candidat=%s)'''
+	    cur.execute(sql, (applicant, applicant))
+	    for row in cur.fetchall():
+	    	jobOffer = {'id': row[0], 
+	    	'position': row[1], 
+	    	'company': row[2], 
+	    	'location': row[3], 
+	    	'datePosted': row[4],
+	    	'description': row[5], 
+	    	'img': row[6]}
+	    	jobOffers.append(jobOffer)
+	    return jsonify({'response': 'success', 'results-for': 'offers','job-offers':jobOffers})
+	except Exception as e:
+            return jsonify({'error': str(e)})
+
+# Favorite offers
+@app.route('/favorites')
+def getFavorites():
+	applicant = request.args.get('applicant')
+	try:
+	    cur = mysql.connect().cursor()
+	    jobOffers = []
+	    cur.execute('''SELECT * FROM offre WHERE id_offre IN (SELECT id_offre FROM favoris)''')
 	    for row in cur.fetchall():
 	    	jobOffer = {'id': row[0], 
 	    	'position': row[1], 
 	    	'company': row[2], 
 	    	'location': row[3], 
 	    	'datePosted': row[4], 
-	    	'contract': row[5], 
-	    	'description': row[6], 
-	    	'img': row[7]}
+	    	'description': row[5], 
+	    	'img': row[6]}
 	    	jobOffers.append(jobOffer)
-	    return jsonify({'response': 'success', 'results-for': 'offers','job-offers':jobOffers})
+	    return jsonify({'response': 'success', 'results-for': 'favorites','job-offers':jobOffers})
 	except Exception as e:
             return jsonify({'error': str(e)})
 
-@app.route('/apply')
-def applyOffer():
-    applicant = request.args.get('applicant')
-    appid = request.args.get('appid')
-    try:
-	    cur = mysql.connect().cursor()
-	    sql = '''insert into candidat_offre(id_candidat, id_offre) values(%s, %s)'''
-	    cur.execute(sql, (applicant, appid))
-	    return jsonify({'response':'success'})
-    except Exception as e:
-            return jsonify({'error': 'Exception'})
-			
-			
-
+# Applications for a user
 @app.route('/applications')
 def getOfferCandidat():
     applicant = request.args.get('applicant')
-    
     try:
 	    cur = mysql.connect().cursor()
-	    sql = '''SELECT * FROM offre , candidat_offre where candidat_offre.id_offre = offre.id_offre and id_candidat = %s'''
+	    sql = '''SELECT offre.*, etat FROM offre, candidat_offre WHERE offre.id_offre=candidat_offre.id_offre AND id_candidat=%s AND etat<>'SUPPRIME' ORDER BY etat'''
 	    cur.execute(sql, (applicant))
 	    candidatApplications = []
 	    for row in cur.fetchall():
-		candidatApplication = {'id': row[0], 
-	    	'position': row[1], 
-	    	'company': row[2], 
-	    	'location': row[3], 
-	    	'datePosted': row[4], 
-	    	'contract': row[5], 
-	    	'description': row[6], 
-	    	'img': row[7]}
-	    	candidatApplications.append(candidatApplication)
+                candidatApplication = {'id': row[0], 
+		'position': row[1], 
+		'company': row[2], 
+		'location': row[3], 
+		'datePosted': row[4], 
+		'description': row[5], 
+		'img': row[6], 
+		'etat': row[7]}
+                candidatApplications.append(candidatApplication)
 	    return jsonify({'response': 'success', 'results-for': 'applications','job-applications':candidatApplications})
     except Exception as e:
 	    return jsonify({'error': str(e)})
+
+# update offer status to SUPPRIME, APP_ATT_RDV, APP_RDV_RECU, APP_REF, APP_ACC
+@app.route('/updateStatus')
+def updateStatus():
+    status = request.args.get('status')
+    applicant = request.args.get('applicant')
+    offer = request.args.get('offer')
+    try:
+	    cur = mysql.connect().cursor()
+	    sql = '''INSERT INTO candidat_offre (id_candidat, id_offre, etat) VALUES(%s, %s, %s) ON DUPLICATE KEY UPDATE etat=%s, id_candidat=%s, id_offre=%s'''
+	    cur.execute(sql, (applicant, offer, status, status, applicant, offer))
+	    return jsonify({'response':'success'})
+    except Exception as e:
+            return jsonify({'error': 'Exception'})
+
+# add an offer to favorites
+@app.route('/addFavorite')
+def addFavorite():
+    applicant = request.args.get('applicant')
+    offer = request.args.get('offer')
+    try:
+	    cur = mysql.connect().cursor()
+	    sql = '''INSERT INTO favoris (id_candidat, id_offre) VALUES(%s, %s)'''
+	    cur.execute(sql, (applicant, offer))
+	    return jsonify({'response':'success'})
+    except Exception as e:
+            return jsonify({'error': 'Exception'})
+
+# Remove offer from favorites
+@app.route('/removeFavorite')
+def removeFavorite():
+	applicant = request.args.get('applicant')
+	offer = request.args.get('offer')
+	try:
+	    cur = mysql.connect().cursor()
+	    sql = '''DELETE FROM favoris WHERE id_candidat=%s AND id_offre=%s'''
+	    cur.execute(sql, (applicant, offer))
+	    return jsonify({'response':'success'})
+	except Exception as e:
+            return jsonify({'error': str(e)})
+
+# Schedule appointment to candidate
+@app.route('/scheduleAppointment')
+def scheduleAppointment():
+	applicant = request.args.get('applicant')
+	recuter = request.args.get('recruter')
+	offer = request.args.get('offer')
+	date = request.args.get('date')
+	place = request.args.get('place')
+	try:
+	    cur = mysql.connect().cursor()
+	    sql = '''INSERT INTO rendez_vous (id_offre, id_candidat, id_recruteur, date_heure, endroit) VALUES(%s, %s, %s, %s, %s)'''
+	    cur.execute(sql, (offer, applicant, recruter, date, place))
+	    return jsonify({'response':'success'})
+	except Exception as e:
+            return jsonify({'error': 'Exception'})
+
+# Get applicants for a recruter
+@app.route('/appointmentRequests')
+def appointmentRequests():
+	recruiter = request.args.get('recruiter')
+	try:
+	    cur = mysql.connect().cursor()
+	    sql = '''select o.position, o.localisation, c.* from (select * from candidat_offre where etat='APP_RDV_ATT' and id_offre in (select id_offre from recruteur_offre where id_recruteur=%s)) as c, offre as o where o.id_offre = c.id_offre'''
+	    cur.execute(sql, (recruiter))
+	    appointmentReq = []
+	    for row in cur.fetchall():
+	    	req = {'position': row[0], 
+	    	'location': row[1], 
+	    	'applicant': row[2], 
+		'offer': row[3]}
+	    	appointmentReq.append(req)
+	    return jsonify({'response': 'success', 'results-for': 'appointments','appointments-requests':appointmentReq})
+	except Exception as e:
+            return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- smartrecruit.sql
+++ smartrecruit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 19, 2019 at 10:49 AM
+-- Generation Time: Mar 05, 2019 at 12:47 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -45,15 +45,31 @@ INSERT INTO `candidat` (`id_candidat`, `nom`, `prenom`, `email`, `telephone`) VA
 
 CREATE TABLE `candidat_offre` (
   `id_candidat` int(11) NOT NULL,
-  `id_offre` int(11) NOT NULL
+  `id_offre` int(11) NOT NULL,
+  `etat` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `candidat_offre`
 --
 
-INSERT INTO `candidat_offre` (`id_candidat`, `id_offre`) VALUES
-(1, 3);
+INSERT INTO `candidat_offre` (`id_candidat`, `id_offre`, `etat`) VALUES
+(2, 1, 'APP_RDV_ATT'),
+(1, 3, 'APP_RDV_ATT'),
+(1, 5, 'APP_RDV_ATT'),
+(1, 1, 'APP_RDV_ATT'),
+(1, 6, 'APP_RDV_ATT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favoris`
+--
+
+CREATE TABLE `favoris` (
+  `id_candidat` int(11) NOT NULL,
+  `id_offre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -67,7 +83,6 @@ CREATE TABLE `offre` (
   `nom_entreprise` varchar(50) DEFAULT NULL,
   `localisation` varchar(50) DEFAULT NULL,
   `date_publication` varchar(10) DEFAULT NULL,
-  `contrat` set('ALTERNANCE','CDD','CDI','STAGE','TEMPS PLEIN','TEMPS PARTIEL','FREELANCE') DEFAULT NULL,
   `descriptif` text,
   `image` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -76,13 +91,69 @@ CREATE TABLE `offre` (
 -- Dumping data for table `offre`
 --
 
-INSERT INTO `offre` (`id_offre`, `position`, `nom_entreprise`, `localisation`, `date_publication`, `contrat`, `descriptif`, `image`) VALUES
-(1, 'Technicien(ne) micro-informatique', 'Humensis', ' Paris 75012', '2019-01-21', 'CDD', 'Sous la responsabilité du Responsable des systèmes d’information de Humensis, vous aurez notamment en charge les missions suivantes: \r\nGestion et préparation des postes de travail utilisateurs (Mac & PC / Téléphonie/ Licences etc…)\nGestion des comptes utilisateurs sous Active Directory\nGestion des droits d \'accès dans le respect des règles de sécurité de l\'entreprise\nGestion du parc Impressions (Copieurs / Imprimantes)\nSupport auprès des utilisateurs sur site ou à distance (matériel, système d exploitation, applications, connexions réseau)Suivi des différents incidents déclarés', 'https://www.puf.com/sites/default/files/kcfinder/images/humensis%20baseline.png'),
-(2, 'Technicien informatique - H/F', 'COPY-TOP', ' Paris 75', '2019-01-21', 'STAGE', 'Rattaché au Responsable d’Exploitation, vos missions au quotidien seront :\nLa gestion et la maintenance du parc informatique (PC/MAC)\nL’installation et la mise à jour des logiciels et applications métiers\nLes connexions RIP et paramétrages des drivers d\'impression (PC/MAC)\nL’assistance aux utilisateurs et la définition des procédures courantes de maintenance\nLa supervision des réseaux LAN et VPN\nLa veille technologique, les préconisations d’amélioration, le reporting d’activité', 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Logo-COPY-TOP.png'),
-(3, 'Product Owner / Designer H/F', 'Jellysmack', ' Levallois-Perret(92)', '2019-01-21', 'CDI,TEMPS PLEIN', 'Aujourd\'hui, nous avons besoin d\'agrandir l\'équipe Produit et de la renforcer avec un Product Owner à très forte sensibilité UI/UX pour améliorer et soutenir le développement des différents produits, en collaboration avec le Product Manager, Product Designer et l\'équipe Tech (développeurs et data scientistes).\nVos missions seront de :\nTraduire les besoins utilisateurs en fonctionnalités,\nProduire les interfaces Sketch,\nSpécifier les fonctionnalités sous formes d\'User Story,\nDéfinir et maintenir le backlog et prioriser les développements au travers de Weekly Goals,\nFaire le suivi des développements et les recettes fonctionnelles et visuelles,\nCommuniquer sur l\'avancement de la réalisation du produit auprès du reste de l\'équipe.', 'https://cdn.welcometothejungle.co/uploads/organization/logo/9100/151516/keli-network.png'),
-(4, 'Technicien d\'exploitation réseaux LAN/WAN H/F', 'Orange', ' Massy(91)', '2019-01-21', 'CDI,TEMPS PLEIN', '   Vous intégrez l\'entité gérant l\'activité des Services Managés en charge de la supervision et de l\'exploitation des Systèmes d\'Informations directement de nos clients grands comptes ;\nAu sein de cette équipe, vous serez assurerez la bonne coordination et prise en charge des problématiques remontées par le client. Vos missions seront les suivantes :\nAdministrer et exploiter les solutions informatiques implémentées chez nos clients,\nGérer les incidents et changements de niveau 1,\nParticiper à la rédaction des procédures d\'exploitation du niveau 1,\nOrienter les escalades vers le niveau 2 ou les services compétents,\nAssurer un reporting de l\'activité déployée chez le client.', 'https://www.castorette.paris/wp-content/uploads/2018/07/orange-logo.jpg'),
-(5, 'STAGE - ASSISTANT IT ANALYST, H/F', 'BNP Paribas Securities Services', 'Pantin(93)', '2019-01-21', 'STAGE', 'Votre activité sera rythmée par l’analyse et le développement d’un outil de suivi interne : l’opportunité d’accéder à un poste à responsabilités dès le début de votre parcours chez BNP Paribas !\n\nVous recevrez les instructions et veillerez à leur bon déroulement :\nDans un premier temps le collaborateur aura en charge d\'effectuer l\'analyse du support fonctionnel relatif à un outil de suivi de l\'activité, utilisée par les responsables IT et MOA, qui est également un outil de suivi des dépenses en liaison avec d’autres logiciels. La version 1 de l\'outil est composée :\nd\'une application développée en PowerBuilder,\nd\'une base de données Sybase.\nDans un second temps, le collaborateur travaillera sur la nouvelle version de l’outil. La refonte en cours s\'effectue en mode AGILE sous JAVA EXTJS et Postgresql.   ', 'https://d29dpxox3qezd.cloudfront.net/uploads/mentor/5b8a6555030bae6d40d6173c/logo.png'),
-(6, 'Administrateur Système et Réseau F/H', 'ACCENGAGE', 'Paris(75)', '2019-01-21', 'ALTERNANCE,STAGE', 'Gestion du parc informatique sous l’assistance du pôle Opérationnel \n\nGestion et résolution des actions de support coté infrastructure \n\nGestion et résolution des tickets et support interne via freshdesk \n\nParticipation au projet de migration bigdata et autres évolutions internes \n\nSous la direction du Référent infra & Production vous interviendrez dans un environnement challengeant sur des technologies innovantes (big data, cloud…) sur une infrastructure à fort trafic. \n\n  ', 'https://www.accengage.com/wp-content/uploads/2014/04/accengage-logo-job-offers-v3-GRAND-FORMAT.png');
+INSERT INTO `offre` (`id_offre`, `position`, `nom_entreprise`, `localisation`, `date_publication`, `descriptif`, `image`) VALUES
+(1, 'Technicien micro-informatique', 'Humensis', ' Paris 75012', '21-01-2019', 'Sous la responsabilité du Responsable des systèmes d’information de Humensis, vous aurez notamment en charge les missions suivantes: \r\nGestion et préparation des postes de travail utilisateurs (Mac & PC / Téléphonie/ Licences etc…)\nGestion des comptes utilisateurs sous Active Directory\nGestion des droits d \'accès dans le respect des règles de sécurité de l\'entreprise\nGestion du parc Impressions (Copieurs / Imprimantes)\nSupport auprès des utilisateurs sur site ou à distance (matériel, système d exploitation, applications, connexions réseau)Suivi des différents incidents déclarés', 'https://www.puf.com/sites/default/files/kcfinder/images/humensis%20baseline.png'),
+(2, 'Technicien informatique', 'COPY-TOP', ' Paris 75', '21-01-2019', 'Rattaché au Responsable d’Exploitation, vos missions au quotidien seront :\nLa gestion et la maintenance du parc informatique (PC/MAC)\nL’installation et la mise à jour des logiciels et applications métiers\nLes connexions RIP et paramétrages des drivers d\'impression (PC/MAC)\nL’assistance aux utilisateurs et la définition des procédures courantes de maintenance\nLa supervision des réseaux LAN et VPN\nLa veille technologique, les préconisations d’amélioration, le reporting d’activité', 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Logo-COPY-TOP.png'),
+(3, 'Product Owner / Designer', 'Jellysmack', ' Levallois-Perret(92)', '01-03-2019', 'Aujourd\'hui, nous avons besoin d\'agrandir l\'équipe Produit et de la renforcer avec un Product Owner à très forte sensibilité UI/UX pour améliorer et soutenir le développement des différents produits, en collaboration avec le Product Manager, Product Designer et l\'équipe Tech (développeurs et data scientistes).\nVos missions seront de :\nTraduire les besoins utilisateurs en fonctionnalités,\nProduire les interfaces Sketch,\nSpécifier les fonctionnalités sous formes d\'User Story,\nDéfinir et maintenir le backlog et prioriser les développements au travers de Weekly Goals,\nFaire le suivi des développements et les recettes fonctionnelles et visuelles,\nCommuniquer sur l\'avancement de la réalisation du produit auprès du reste de l\'équipe.', 'https://cdn.welcometothejungle.co/uploads/organization/logo/9100/151516/keli-network.png'),
+(4, 'Technicien d\'exploitation réseaux', 'Orange', ' Massy(91)', '17-01-2019', '   Vous intégrez l\'entité gérant l\'activité des Services Managés en charge de la supervision et de l\'exploitation des Systèmes d\'Informations directement de nos clients grands comptes ;\nAu sein de cette équipe, vous serez assurerez la bonne coordination et prise en charge des problématiques remontées par le client. Vos missions seront les suivantes :\nAdministrer et exploiter les solutions informatiques implémentées chez nos clients,\nGérer les incidents et changements de niveau 1,\nParticiper à la rédaction des procédures d\'exploitation du niveau 1,\nOrienter les escalades vers le niveau 2 ou les services compétents,\nAssurer un reporting de l\'activité déployée chez le client.', 'https://www.castorette.paris/wp-content/uploads/2018/07/orange-logo.jpg'),
+(5, 'STAGE - ASSISTANT IT ANALYST', 'BNP Paribas Securities Services', 'Pantin(93)', '21-01-2019', 'Votre activité sera rythmée par l’analyse et le développement d’un outil de suivi interne : l’opportunité d’accéder à un poste à responsabilités dès le début de votre parcours chez BNP Paribas !\n\nVous recevrez les instructions et veillerez à leur bon déroulement :\nDans un premier temps le collaborateur aura en charge d\'effectuer l\'analyse du support fonctionnel relatif à un outil de suivi de l\'activité, utilisée par les responsables IT et MOA, qui est également un outil de suivi des dépenses en liaison avec d’autres logiciels. La version 1 de l\'outil est composée :\nd\'une application développée en PowerBuilder,\nd\'une base de données Sybase.\nDans un second temps, le collaborateur travaillera sur la nouvelle version de l’outil. La refonte en cours s\'effectue en mode AGILE sous JAVA EXTJS et Postgresql.   ', 'https://d29dpxox3qezd.cloudfront.net/uploads/mentor/5b8a6555030bae6d40d6173c/logo.png'),
+(6, 'Administrateur Système et Réseau', 'ACCENGAGE', 'Paris(75)', '18-02-2019', 'Gestion du parc informatique sous l’assistance du pôle Opérationnel \n\nGestion et résolution des actions de support coté infrastructure \n\nGestion et résolution des tickets et support interne via freshdesk \n\nParticipation au projet de migration bigdata et autres évolutions internes \n\nSous la direction du Référent infra & Production vous interviendrez dans un environnement challengeant sur des technologies innovantes (big data, cloud…) sur une infrastructure à fort trafic. \n\n  ', 'https://www.accengage.com/wp-content/uploads/2014/04/accengage-logo-job-offers-v3-GRAND-FORMAT.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recruteur`
+--
+
+CREATE TABLE `recruteur` (
+  `id_recruteur` int(11) NOT NULL,
+  `nom` varchar(80) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telephone` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recruteur`
+--
+
+INSERT INTO `recruteur` (`id_recruteur`, `nom`, `prenom`, `email`, `telephone`) VALUES
+(1, 'Tresh', 'Holden', 't.holden@gmail.com', '06787878');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recruteur_offre`
+--
+
+CREATE TABLE `recruteur_offre` (
+  `id_recruteur` int(11) NOT NULL,
+  `id_offre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recruteur_offre`
+--
+
+INSERT INTO `recruteur_offre` (`id_recruteur`, `id_offre`) VALUES
+(1, 1),
+(1, 2),
+(1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rendez_vous`
+--
+
+CREATE TABLE `rendez_vous` (
+  `id_rdv` int(11) NOT NULL,
+  `id_offre` int(11) NOT NULL,
+  `id_candidat` int(11) NOT NULL,
+  `id_recruteur` int(11) NOT NULL,
+  `date_heure` varchar(20) NOT NULL,
+  `endroit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -102,10 +173,37 @@ ALTER TABLE `candidat_offre`
   ADD KEY `id_offre` (`id_offre`);
 
 --
+-- Indexes for table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD PRIMARY KEY (`id_candidat`,`id_offre`),
+  ADD KEY `id_offre` (`id_offre`);
+
+--
 -- Indexes for table `offre`
 --
 ALTER TABLE `offre`
   ADD PRIMARY KEY (`id_offre`);
+
+--
+-- Indexes for table `recruteur`
+--
+ALTER TABLE `recruteur`
+  ADD PRIMARY KEY (`id_recruteur`);
+
+--
+-- Indexes for table `recruteur_offre`
+--
+ALTER TABLE `recruteur_offre`
+  ADD PRIMARY KEY (`id_offre`),
+  ADD KEY `id_offre` (`id_offre`);
+
+--
+-- Indexes for table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  ADD PRIMARY KEY (`id_rdv`),
+  ADD KEY `id_offre` (`id_offre`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -121,3 +219,13 @@ ALTER TABLE `candidat`
 --
 ALTER TABLE `offre`
   MODIFY `id_offre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `recruteur`
+--
+ALTER TABLE `recruteur`
+  MODIFY `id_recruteur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  MODIFY `id_rdv` int(11) NOT NULL AUTO_INCREMENT;
